017953b363735c20ef6619a86925655568004694
m0-ar <s176492@student.dtu.dk>
2021-10-20 20:38:57 +0200

    Type the right base url

--- app/src/main/java/com/example/mycryptoapp/Constants.kt
+++ app/src/main/java/com/example/mycryptoapp/Constants.kt
@@ -4,8 +4,8 @@ class Constants {
 
     companion object {
 
-        const val BASE_URL_ASSETS = "https://api.coincap.io/v2/assets/"
-        const val BASE_URL_ICONS = "https://static.coincap.io/assets/icons/"
+        const val BASE_URL_ASSETS = "https://api.coincap.io"
+        const val BASE_URL_ICONS = "https://static.coincap.io"
         const val API_KEY = "f7a3614c-90b7-437e-a163-c3601ea555cf"
 
     }
