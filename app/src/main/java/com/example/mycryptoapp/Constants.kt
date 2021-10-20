821b3d7d5c126980ff1c1253036b8e57dd36b73f
m0-ar <s176492@student.dtu.dk>
2021-10-20 20:34:49 +0200

    Add api-key and base-url endpoint to Constants

---
+++ app/src/main/java/com/example/mycryptoapp/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.mycryptoapp
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL_ASSETS = "https://api.coincap.io/v2/assets/"
+        const val BASE_URL_ICONS = "https://static.coincap.io/assets/icons/"
+        const val API_KEY = "f7a3614c-90b7-437e-a163-c3601ea555cf"
+
+    }
+
+}
