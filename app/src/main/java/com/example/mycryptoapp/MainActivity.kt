06c897f7c5b3b8310823de43ef622cbb7bce2f03
m0-ar <s176492@student.dtu.dk>
2021-10-20 14:45:46 +0200

    Navigate throw bottom navigation bar and change theme's color.

--- app/src/main/java/com/example/mycryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/mycryptoapp/MainActivity.kt
@@ -2,12 +2,37 @@ package com.example.mycryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navController = findNavController(R.id.navHostFragmentContainer)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.cryptosFragment,
+                R.id.favoriteCryptosFragment,
+                R.id.myPortfolioFragment,
+                R.id.cryptoAdviceFragment
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
