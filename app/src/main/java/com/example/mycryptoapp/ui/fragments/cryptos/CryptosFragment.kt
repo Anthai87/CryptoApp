599bfa72b2022314cdc0c4ff12b303c71e31e317
m0-ar <s176492@student.dtu.dk>
2021-10-21 16:04:26 +0200

    Try fetch api data

--- app/src/main/java/com/example/mycryptoapp/ui/fragments/cryptos/CryptosFragment.kt
+++ app/src/main/java/com/example/mycryptoapp/ui/fragments/cryptos/CryptosFragment.kt
@@ -5,22 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mycryptoapp.MainViewModel
 import com.example.mycryptoapp.R
+import com.example.mycryptoapp.adapters.CryptosAdapter
+import com.example.mycryptoapp.util.NetworkResult
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_cryptos.view.*
 
-
+@AndroidEntryPoint
 class CryptosFragment : Fragment() {
+
+    private lateinit var mMainViewModel: MainViewModel
+    private val mAdapter by lazy { CryptosAdapter() }
+    private lateinit var mView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_cryptos, container, false)
+        mView = inflater.inflate(R.layout.fragment_cryptos, container, false)
 
-        view.shimmer_recycler_view.showShimmer()
+        setupRecyclerView()
+        requestApiData()
 
-        return view
+        return mView
+    }
+
+    private fun setupRecyclerView() {
+        mView.recyclerview.adapter = mAdapter
+        mView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect() {
+        mView.recyclerview.showShimmer()
+    }
+
+    private fun requestApiData() {
+        mMainViewModel.getAssets()
+        mMainViewModel.assetsResponse.observe(viewLifecycleOwner, { response ->
+            when(response) {
+                is NetworkResult.Success -> {
+                    hideShimmerEffect()
+                    response.data?.let { mAdapter.setData(it) }
+                }
+            }
+        })
+    }
+
+    private fun hideShimmerEffect() {
+        mView.recyclerview.hideShimmer()
     }
 
 }
