b631067ddf52c16eead16962130e8c21a65ed213
m0-ar <s176492@student.dtu.dk>
2021-10-21 13:05:02 +0200

    Create logic for price and percent in CryptosRowBinding

---
+++ app/src/main/java/com/example/mycryptoapp/bindingadapters/CryptosRowBinding.kt
@@ -0,0 +1,60 @@
+package com.example.mycryptoapp.bindingadapters
+
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.example.mycryptoapp.R
+
+class CryptosRowBinding {
+
+    companion object {
+
+        @BindingAdapter("reformatPriceUsd")
+        @JvmStatic
+        fun reformatPriceUsd(textView: TextView, priceUsd: String) {
+            textView.text = addDollarSymbol(setTwoDecimal(priceUsd))
+        }
+
+        private fun setTwoDecimal(price: String): String {
+            return String.format("$%.2f", price.toDouble())
+        }
+
+        private fun addDollarSymbol(price: String): String {
+            return "$$price"
+        }
+
+        @BindingAdapter("reformatChangePercent24Hr")
+        @JvmStatic
+        fun reformatChangePercent24Hr(textView: TextView, changePercent24Hr: String) {
+            textView.text = setColor(textView, addPercentSign(addPlusOrMinusSign(changePercent24Hr))).toString()
+
+        }
+
+        private fun addPlusOrMinusSign(percent: String): String {
+            return if (percent.toDouble() > 0) "+$percent" else "-$percent"
+        }
+
+        private fun addPercentSign(addPlusOrMinusSign: String): String {
+            return "$addPlusOrMinusSign%"
+        }
+
+        private fun setColor(textView: TextView, changePercent24Hr: String) {
+            return if (changePercent24Hr.toDouble() > 0)
+                    textView.setTextColor(
+                        ContextCompat.getColor(
+                            textView.context,
+                            R.color.green
+                        )
+                    )
+                 else
+                    textView.setTextColor(
+                        ContextCompat.getColor(
+                            textView.context,
+                            R.color.red
+                        )
+                    )
+        }
+
+    }
+
+}
