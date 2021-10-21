ed8853f3c4e6fdc675abfd844517f074357e2919
m0-ar <s176492@student.dtu.dk>
2021-10-21 09:09:12 +0200

    Create NetworkModule -> which show how Hilt provides CryptoAssetsApi by following these steps:
      1- To provide 'CryptoAssetsApi' in 'provideApiService', so we need to satisfy retrofit dependency.
      2- To satisfy retrofit dependencies need 'provideRetrofitInstance' okHttpClient and gsonConverterFactory
      3- Tell Hilt how to create these two dependencies 'provideConverterFactory' and 'provideHttpClient'

---
+++ app/src/main/java/com/example/mycryptoapp/di/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.example.mycryptoapp.di
+
+import com.example.mycryptoapp.Constants.Companion.BASE_URL_ASSETS
+import com.example.mycryptoapp.CryptoAssetsApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ActivityComponent::class)
+object NetworkModule {
+
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL_ASSETS)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): CryptoAssetsApi {
+        return retrofit.create(CryptoAssetsApi::class.java)
+    }
+
+}
