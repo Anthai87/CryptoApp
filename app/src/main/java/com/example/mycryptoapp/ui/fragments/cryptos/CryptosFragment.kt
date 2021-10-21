0f6475bfdf9b7fed86d0ad361c41abc4452de00e
m0-ar <s176492@student.dtu.dk>
2021-10-21 16:35:05 +0200

    Invoke MainViewModel

--- app/src/main/java/com/example/mycryptoapp/ui/fragments/cryptos/CryptosFragment.kt
+++ app/src/main/java/com/example/mycryptoapp/ui/fragments/cryptos/CryptosFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mycryptoapp.MainViewModel
 import com.example.mycryptoapp.R
@@ -26,6 +27,8 @@ class CryptosFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_cryptos, container, false)
+
+        mMainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
 
         setupRecyclerView()
         requestApiData()
