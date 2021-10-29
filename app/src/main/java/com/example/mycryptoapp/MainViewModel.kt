7acf68f35ef9c9db3d0f1fc9ac19a0feb7ebf4b5
m0-ar <s176492@student.dtu.dk>
2021-10-29 12:37:16 +0200

    Make hasInternetConnection() and increase minSdk to 23

--- app/src/main/java/com/example/mycryptoapp/MainViewModel.kt
+++ app/src/main/java/com/example/mycryptoapp/MainViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.mycryptoapp
 
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mycryptoapp.data.Repository
 import com.example.mycryptoapp.models.Assets
 import com.example.mycryptoapp.util.NetworkResult
+import dagger.hilt.android.internal.Contexts.getApplication
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,7 +19,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: Repository,
-) : ViewModel() {
+    application: Application
+) : AndroidViewModel(application) {
 
     // Todo: Handle network and response error later
 
@@ -23,6 +30,21 @@ class MainViewModel @Inject constructor(
         val response = repository.remote.getAssets()
         val assets = response.body()
         assetsResponse.value = NetworkResult.Success(assets!!)
+    }
+
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getApplication<Application>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
     }
 
 }
