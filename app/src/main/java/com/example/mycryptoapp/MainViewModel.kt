9bddc59ab27e2974d5f93b6818aa392cf154a70a
m0-ar <s176492@student.dtu.dk>
2021-10-29 13:14:41 +0200

    Handle network and response error

--- app/src/main/java/com/example/mycryptoapp/MainViewModel.kt
+++ app/src/main/java/com/example/mycryptoapp/MainViewModel.kt
@@ -6,15 +6,14 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mycryptoapp.data.Repository
 import com.example.mycryptoapp.models.Assets
 import com.example.mycryptoapp.util.NetworkResult
-import dagger.hilt.android.internal.Contexts.getApplication
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import retrofit2.Response
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
@@ -22,14 +21,45 @@ class MainViewModel @Inject constructor(
     application: Application
 ) : AndroidViewModel(application) {
 
-    // Todo: Handle network and response error later
-
     var assetsResponse: MutableLiveData<NetworkResult<Assets>> = MutableLiveData()
 
     fun getAssets() = viewModelScope.launch {
-        val response = repository.remote.getAssets()
-        val assets = response.body()
-        assetsResponse.value = NetworkResult.Success(assets!!)
+        getAssetsSafeCall()
+    }
+
+    private suspend fun getAssetsSafeCall() {
+        assetsResponse.value = NetworkResult.Loading()
+        if (hasInternetConnection()) {
+            try {
+                val response = repository.remote.getAssets()
+                assetsResponse.value = handleCryptoAssetsResponse(response)
+            } catch (e: Exception) {
+                assetsResponse.value = NetworkResult.Error("Assets not found.")
+            }
+        } else {
+            assetsResponse.value = NetworkResult.Error("No Internet Connection.")
+        }
+    }
+
+    private fun handleCryptoAssetsResponse(response: Response<Assets>): NetworkResult<Assets>? {
+        when {
+            response.message().toString().contains("timeout") -> {
+                return NetworkResult.Error("Timeout")
+            }
+            response.code() == 402 -> {
+                return NetworkResult.Error("API Key Limited.")
+            }
+            response.body()!!.cryptos.isNullOrEmpty() -> {
+                return NetworkResult.Error("API Key Limited.")
+            }
+            response.isSuccessful -> {
+                val cryptoAssets = response.body()
+                return NetworkResult.Success(cryptoAssets!!)
+            }
+            else -> {
+                return NetworkResult.Error(response.message())
+            }
+        }
     }
 
     private fun hasInternetConnection(): Boolean {
