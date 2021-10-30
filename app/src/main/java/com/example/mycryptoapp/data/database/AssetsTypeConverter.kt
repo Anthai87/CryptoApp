5b708a45e8196c286cebe051840a9764dfe5e249
m0-ar <s176492@student.dtu.dk>
2021-10-30 10:04:46 +0200

    Convert data to json to store in json form

---
+++ app/src/main/java/com/example/mycryptoapp/data/database/AssetsTypeConverter.kt
@@ -0,0 +1,23 @@
+package com.example.mycryptoapp.data.database
+
+import androidx.room.TypeConverter
+import com.example.mycryptoapp.models.Assets
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class AssetsTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun assetsToString(assets: Assets): String {
+        return gson.toJson(assets)
+    }
+
+    @TypeConverter
+    fun stringToAssets(data: String): Assets {
+        val listType = object : TypeToken<Assets>() {}.type
+        return gson.fromJson(data, listType)
+    }
+
+}
