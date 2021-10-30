3b182d3416f4351041ea1f476abfc6cbcbff647e
m0-ar <s176492@student.dtu.dk>
2021-10-30 09:58:52 +0200

    Create AssetsDao

---
+++ app/src/main/java/com/example/mycryptoapp/data/database/AssetsDao.kt
@@ -0,0 +1,18 @@
+package com.example.mycryptoapp.data.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AssetsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAssets(assetsEntity: AssetsEntity)
+
+    @Query("SELECT * FROM assets_table ORDER BY id ASC")
+    fun readAssets(): Flow<List<AssetsEntity>>
+
+}
