38151e1b8ceb37e489c8f445143f52375cdadb55
m0-ar <s176492@student.dtu.dk>
2021-10-30 11:17:08 +0200

    bug fixed

--- app/src/main/java/com/example/mycryptoapp/data/database/AssetsDatabase.kt
+++ app/src/main/java/com/example/mycryptoapp/data/database/AssetsDatabase.kt
@@ -2,12 +2,14 @@ package com.example.mycryptoapp.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     entities = [AssetsEntity::class],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(AssetsTypeConverter::class)
 abstract class AssetsDatabase: RoomDatabase() {
 
     abstract fun assetsDao(): AssetsDao
