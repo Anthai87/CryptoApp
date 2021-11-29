b620cb7974c0cb7f3981639e0ae619b23b149075
m0-ar <s176492@student.dtu.dk>
2021-11-29 22:31:00 +0100

    Add splashscreen

--- app/src/main/java/com/example/mycryptoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/mycryptoapp/ui/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_MyCryptoApp)
         setContentView(R.layout.activity_main)
 
         navController = findNavController(R.id.navHostFragmentContainer)
