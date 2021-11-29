7f3027ecb5804b99225eab3da677c006d01c579a
m0-ar <s176492@student.dtu.dk>
2021-11-29 21:44:59 +0100

    Fix webView

--- app/src/main/java/com/example/mycryptoapp/ui/fragments/cryptooverview/CryptoInstructionsFragment.kt
+++ app/src/main/java/com/example/mycryptoapp/ui/fragments/cryptooverview/CryptoInstructionsFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.example.mycryptoapp.R
 import com.example.mycryptoapp.models.Crypto
 import com.example.mycryptoapp.util.Constants.Companion.CRYPTO_KEY
+import kotlinx.android.synthetic.main.fragment_crypto_instructions.*
 import kotlinx.android.synthetic.main.fragment_crypto_instructions.view.*
 
 
@@ -23,6 +26,15 @@ class CryptoInstructionsFragment : Fragment() {
 
         val args = arguments
         val myBundle: Crypto? = args?.getParcelable(CRYPTO_KEY)
+
+        // https://developer.android.com/guide/webapps/webview
+        val webView: WebView = view.instructions_webView
+        webView.settings.domStorageEnabled = true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.loadsImagesAutomatically = true
+        webView.settings.useWideViewPort = true
+        webView.settings.builtInZoomControls = true
 
         view.instructions_webView.webViewClient = object : WebViewClient() {}
         val websiteUrl: String = myBundle!!.explorer
