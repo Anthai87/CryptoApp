8cb9919669232177c9f896c3ad3557d875c8dc7f
anthai87 <s141479@student.dtu.dk>
2021-11-30 13:41:05 +0100

    implementet setUpChart within FavoriteCryptosFragment.kt class.

--- app/src/main/java/com/example/mycryptoapp/ui/fragments/favorites/CryptoSparkAdapter.kt
+++ app/src/main/java/com/example/mycryptoapp/ui/fragments/favorites/CryptoSparkAdapter.kt
@@ -1,17 +1,29 @@
 package com.example.mycryptoapp.ui.fragments.favorites
 
-import com.example.mycryptoapp.models.Assets
 import com.example.mycryptoapp.models.Crypto
 import com.robinhood.spark.SparkAdapter
 
-class CryptoSparkAdapter(private val dailyData: List<Crypto>): SparkAdapter() {
+class CryptoSparkAdapter(private val dailyData: List<Crypto>, private val baseLineValue: String?) :
+    SparkAdapter() {
     override fun getY(index: Int): Float {
-        val chosenDayData = dailyData[index]
-        return chosenDayData.marketCapUsd.toFloat()
+        return dailyData[index].priceUsd.toFloat()
     }
 
-    override fun getItem(index: Int) = dailyData[index]
+    override fun getItem(index: Int): Crypto {
+        return dailyData[index]
 
-    override fun getCount() = dailyData.size
+    }
 
+    override fun getCount(): Int {
+        return dailyData.size
+    }
+
+    override fun hasBaseLine(): Boolean {
+        return true
+    }
+
+    override fun getBaseLine(): Float {
+        return baseLineValue?.toFloat() ?: super.getBaseLine()
+    }
 }
+
--- app/src/main/java/com/example/mycryptoapp/ui/fragments/favorites/FavoriteCryptosFragment.kt
+++ app/src/main/java/com/example/mycryptoapp/ui/fragments/favorites/FavoriteCryptosFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mycryptoapp.ui.fragments.favorites
 
+import android.graphics.DashPathEffect
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,9 +15,9 @@ import kotlinx.android.synthetic.main.historical_chart_module.*
 
 
 class FavoriteCryptosFragment : Fragment() {
-    private lateinit var adapter: CryptoSparkAdapter
-    private lateinit var currentlyShownData: List<Crypto>
 
+    // private lateinit var adapter: CryptoSparkAdapter
+    // private lateinit var currentlyShownData: List<Crypto>
 
 
     override fun onCreateView(
@@ -27,16 +28,18 @@ class FavoriteCryptosFragment : Fragment() {
         return inflater.inflate(R.layout.historical_chart_module, container, false)
     }
 
-    private fun updateDisplayWithData(dailyData: List<Crypto>) {
-
-        val adapter = CryptoSparkAdapter(dailyData)
-        historicalChartView.adapter = adapter
-
+    private fun setupChart(dailyDataListPair: Pair<List<Crypto>, Crypto?>) {
+        historicalChartView.adapter = CryptoSparkAdapter(dailyDataListPair.first, dailyDataListPair.second?.priceUsd)
 
 
         // updateInforFordate(dailyData.last())
 
+        val baseLinePaint = historicalChartView.baseLinePaint
+        val dashPathEffect = DashPathEffect(floatArrayOf(10.0f, 2.0f), 0f)
+        baseLinePaint.pathEffect = dashPathEffect
+
     }
+
     private fun updateInforFordate(cryptoData: Crypto) {
 
 
