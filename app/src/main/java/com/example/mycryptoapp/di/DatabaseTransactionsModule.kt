7296bc9451e0eaba64d1b2071cf9f7c05aeb644d
anthai87 <s141479@student.dtu.dk>
2021-12-01 17:17:26 +0100

    DatabaseTransactionsModule.kt

---
+++ app/src/main/java/com/example/mycryptoapp/di/DatabaseTransactionsModule.kt
@@ -0,0 +1,32 @@
+package com.example.mycryptoapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.mycryptoapp.data.database.assets.AssetsDatabase
+import com.example.mycryptoapp.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseTransactionsModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        AssetsDatabase::class.java,
+        Constants.DATABASE_NAME_SQLITE
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: AssetsDatabase) = database.assetsDao()
+}
