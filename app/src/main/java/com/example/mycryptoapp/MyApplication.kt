652d706b1ccd2c3fa8aaae6ab9e6024a7d913788
m0-ar <s176492@student.dtu.dk>
2021-12-06 14:19:53 +0100

    Delete the cache database when the app is opening so we get the last update

--- app/src/main/java/com/example/mycryptoapp/MyApplication.kt
+++ app/src/main/java/com/example/mycryptoapp/MyApplication.kt
@@ -1,8 +1,15 @@
 package com.example.mycryptoapp
 
 import android.app.Application
+import com.example.mycryptoapp.util.Constants.Companion.DATABASE_NAME
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        deleteDatabase(DATABASE_NAME);
+    }
 }
